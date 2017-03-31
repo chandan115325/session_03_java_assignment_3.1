2ad90fd675d4506539b5e4d6b19804adb0b87b69
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-03-31 18:53:14 +0530

    no message

--- src/TestDesmo.java
+++ src/TestDesmo.java
@@ -1,9 +1,14 @@
 
-public class TestDesmo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		public class TestDesmo{
+			static{
+			print(10);
+			}
+			static void print (int x) {
+			System.out.println (x);
+			System.exit(0);
+			}
+			
+	
 
 }
